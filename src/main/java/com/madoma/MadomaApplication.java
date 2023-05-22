e98392855af7ddf5bc6f5a7798aad0dc701cca6c
Sanzhar Taalaibek uulu <taalaibekuulusanzhar@gmail.com>
2023-05-22 17:51:50 +0600

    start project

---
+++ src/main/java/com/madoma/MadomaApplication.java
@@ -0,0 +1,13 @@
+package com.madoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadomaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madoma/MadomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
